a0b92117d0bfef96535b44a97c756cf7a9fb76f8
Akinwale Ariwodola <akinwale@gmail.com>
2019-11-14 22:33:14 +0100

    add extra flag for hiding the navigation bar

--- src/main/java/io/lbry/browser/reactmodules/UtilityModule.java
+++ src/main/java/io/lbry/browser/reactmodules/UtilityModule.java
@@ -115,7 +115,8 @@ public class UtilityModule extends ReactContextBaseJavaModule {
             activity.runOnUiThread(new Runnable() {
                 public void run() {
                     View decorView = activity.getWindow().getDecorView();
-                    decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                    decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN |
+                                                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
                                                     View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
                                                     View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
                                                     View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
